976df119a5592615a660c625ec123e27d948562b
rydham1210 <rydhamsingla4415.rs@gmail.com>
2026-03-21 19:24:20 +0530

    Add files via upload

---
+++ DAY49/First.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    if (root == NULL) return createNode(val);
+
+    if (val < root->val)
+        root->left = insertIntoBST(root->left, val);
+    else
+        root->right = insertIntoBST(root->right, val);
+
+    return root;
+}
+
+void inorder(struct TreeNode* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->val);
+    inorder(root->right);
+}
+
+int main() {
+    int n, val;
+    struct TreeNode* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insertIntoBST(root, val);
+    }
+
+    inorder(root);
+
+    return 0;
+}
