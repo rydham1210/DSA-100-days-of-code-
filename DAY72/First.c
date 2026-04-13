950ade382645c80d3d5c85bb7768bce3c9754bc5
rydham1210 <rydhamsingla4415.rs@gmail.com>
2026-04-13 22:21:12 +0530

    Add files via upload

---
+++ DAY72/First.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s", s);
+
+    int freq[26] = {0};
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        int index = s[i] - 'a';
+        freq[index]++;
+
+        if (freq[index] == 2) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+
+    printf("-1\n");
+    return 0;
+}
