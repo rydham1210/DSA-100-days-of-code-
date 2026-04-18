97b20c85fe23d77e14b0007e134a1af5fe18393c
rydham1210 <rydhamsingla4415.rs@gmail.com>
2026-04-18 13:06:58 +0530

    Add files via upload

---
+++ DAY77/First.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int visited[1001];
+int adj[1001][1001];
+int n, m;
+
+void dfs(int node) {
+    visited[node] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    dfs(1);
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED");
+    return 0;
+}
